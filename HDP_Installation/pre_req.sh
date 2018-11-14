70f7848d16862d816173c60ff596f5d18215be64
Cesar Rodrigues <cesarcrfilho@gmail.com>
2018-11-14 12:08:45 -0500

    Create pre_req.sh
    - uploading the prerequisites script

---
+++ HDP_Installation/pre_req.sh
@@ -0,0 +1,8 @@
+hostnamectl set-hostname master1.hadoop.com # set your hostname here
+echo "vm.swappiness = 10" >> /etc/sysctl.conf
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+yum -y install ntp
+systemctl enable ntpd
+systemctl start ntpd
+echo "echo never > /sys/kernel/mm/transparent_hugepage/enabled " >> /etc/rc.local
+echo "echo never > /sys/kernel/mm/transparent_hugepage/defrag " >> /etc/rc.local
